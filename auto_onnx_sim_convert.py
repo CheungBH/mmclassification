b8bc2fbe855d14e0caefb7f6ecae3a1b85460ba0
SAIL-LAB155 <saillab155@gmail.com>
2020-12-26 16:39:27 +0800

    add onnx and ncnn conversion

---
+++ auto_onnx_sim_convert.py
@@ -0,0 +1,19 @@
+import os
+
+model_config = {
+    "work_dirs/drown_walk-mobile/latest.pth":
+        "customized_cfg/drown_2cls/mobilenet_v2.py",
+    "work_dirs/drown_walk-shuffle/latest.pth": "customized_cfg/drown_2cls/shufflenet_v2.py"
+}
+
+for idx, (model, config) in enumerate(model_config.items()):
+    print("---------------------Converting model {}---------------------------".format(idx))
+    onnx_path = os.path.join("/".join(model.split("/")[:-1]), "model.onnx")
+    onnx_cmd = "python tools/pytorch2onnx.py {} --checkpoint {} --verify".format(config, model)
+    print(onnx_cmd)
+    os.system(onnx_cmd)
+    sim_cmd = "python -m onnxsim {} {}".format(onnx_path, onnx_path.replace("onnx", "_sim.onnx"))
+    print(sim_cmd)
+    os.system(sim_cmd)
+
+
