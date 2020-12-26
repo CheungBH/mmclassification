d25f68d3e7c173d91ebd6f086754852e729d5dfd
SAIL-LAB155 <saillab155@gmail.com>
2020-12-26 17:15:56 +0800

    modify convert

--- auto_onnx_sim_convert.py
+++ auto_onnx_sim_convert.py
@@ -9,7 +9,7 @@ model_config = {
 for idx, (model, config) in enumerate(model_config.items()):
     print("---------------------Converting model {}---------------------------".format(idx))
     onnx_path = os.path.join("/".join(model.split("/")[:-1]), "model.onnx")
-    onnx_cmd = "python tools/pytorch2onnx.py {} --checkpoint {} --verify".format(config, model)
+    onnx_cmd = "python tools/pytorch2onnx.py {} --checkpoint {} --verify --output-file {}".format(config, model, onnx_path)
     print(onnx_cmd)
     os.system(onnx_cmd)
     sim_cmd = "python -m onnxsim {} {}".format(onnx_path, onnx_path.replace("onnx", "_sim.onnx"))
