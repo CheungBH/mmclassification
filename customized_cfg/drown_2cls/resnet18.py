84488c7c90cbb3589a1694819fdd35e2bb24ae30
cheungbh <534660436@qq.com>
2020-12-26 16:02:54 +0800

    modify resnet18

--- customized_cfg/drown_2cls/resnet18.py
+++ customized_cfg/drown_2cls/resnet18.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../configs/_base_/models/resnet18_drown.py',
+    '../../configs/_base_/models/2cls/resnet18.py',
     '../../configs/_base_/datasets/drown_2cls.py',
     '../../configs/_base_/schedules/drown.py',
     '../../configs/_base_/default_runtime.py'
